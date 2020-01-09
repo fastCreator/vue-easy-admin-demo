01cd9645b03429a61c6c3c8a897a936407fa1692
Your Name <you@example.com>
2020-01-09 23:14:05 +0800

    .

---
+++ config.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Vue Admin',//change自动变化
+  setting: {
+    logo: true,
+    fixedHeader: true,
+    tagView: true,
+    theme: '',
+    show: true
+  }
+
+}
---
+++ src/external-link/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <!-- <el-button v-permission="add">新增</el-button> -->
+    <!-- <div>{{ data }}</div> -->
+  </div>
+</template>
+<script>
+import $ from 'jquery'
+export default {
+  created () {
+    // checkPermission
+    // this.API.getTable()
+  }
+}
+</script>
