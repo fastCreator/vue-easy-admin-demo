166453c81502d868fd1caa32e2179214baedb447
zhangguo <zhangguo@inke.cn>
2020-01-10 18:55:00 +0800

    .

--- src/external-link/index.vue
+++ src/external-link/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an link page</h1>
     <!-- <el-button v-permission="add">新增</el-button> -->
     <!-- <div>{{ data }}</div> -->
   </div>
 </template>
 <script>
-import $ from 'jquery'
 export default {
   created () {
     // checkPermission
