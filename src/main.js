e47260f1b461452b90db524c040820a4ce656afb
zhangguo <zhangguo@inke.cn>
2020-01-14 16:51:54 +0800

    demo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import vueElementUiExpand from 'vue-element-ui-expand'
-export default Vue => {
+import vueElementUiExpand from '../../vue-element-ui-expand'
+export default (Vue, router, store) => {
+  // console.log(Vue, router, store)
   Vue.use(vueElementUiExpand)
 }
