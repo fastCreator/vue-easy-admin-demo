4d7a174553834c410dfb5146aef652a53dc134a2
zhangguo <zhangguo@inke.cn>
2020-01-15 18:39:37 +0800

    准备permisson

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import vueElementUiExpand from '../../vue-element-ui-expand'
-export default (Vue, router, store) => {
-  // console.log(Vue, router, store)
+export default async ({ Vue, router, store, nav, api }) => {
+  //请求用户信息
+  let user = api.get('/userInfo').then(d => d.result)
+  store.setUser(user)
+  //修改菜单
+  nav[0].title = '修改菜单'
+  //添加组件库
   Vue.use(vueElementUiExpand)
 }
