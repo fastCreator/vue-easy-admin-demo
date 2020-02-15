9cea5a6e7cc61e302c9f7536a86ecb17a7f1c7a5
Your Name <you@example.com>
2020-02-15 23:27:37 +0800

    .

--- container.js
+++ container.js
@@ -8,12 +8,5 @@ module.exports = {
     })
   },
   configureWebpack: {
-    devServer: {
-      proxy: {
-        '^/service': {
-          target: 'http://39.105.156.22:8095'
-        }
-      }
-    }
   }
 }
